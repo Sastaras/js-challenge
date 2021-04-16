60c1c3939e5fe275056b5c5096515c2530ac90ee
Gaël Sudour-Jeannin <gael.sudourjeannin@gmail.com>
2021-04-16 09:36:11 +0200

    copy-paste

---
+++ copy-paste/main.js
@@ -0,0 +1,7 @@
+const buttonCopy = document.getElementById("button-copy") ;
+const copyText = document.getElementById("copy-text") ;
+
+buttonCopy.addEventListener("click", function() {
+    copyText.select();
+    document.execCommand("copy")  ;
+});
